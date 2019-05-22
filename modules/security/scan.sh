05cc121334b7ede499a7cbe6a3cdfb8cfc68aada
Trenton Potgieter <darkreapyre@gmail.com>
2019-05-22 13:55:21 -0700

    Final tweaks

--- modules/security/scan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DOCKER_LOGIN=`aws ecr get-login --no-include-email`
-PASSWORD=`echo $DOCKER_LOGIN | cut -d' ' -f6`
-IMAGE_URI=$1
-DOCKER_USER=AWS DOCKER_PASSWORD=${PASSWORD} CLAIR_ADDR=52.42.3.7:6060 ./klar $IMAGE_URI
